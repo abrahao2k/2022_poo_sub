796aaa38ce1cf055dd468ffbc91a7048c1ec304d
abrahao2k <117474324+abrahao2k@users.noreply.github.com>
2022-12-08 10:28:17 -0300

    Add files via upload

---
+++ classe1.py
@@ -0,0 +1,13 @@
+class MinhaClasse:
+    nome = "ana"
+    idade = 5
+
+###############################
+    
+objeto1 = MinhaClasse()
+print(objeto1.nome)
+print(objeto1.idade)
+
+objeto1.nome = "ana clara"
+objeto1.idade = 6
+print(objeto1.nome, objeto1.idade)
---
+++ classe2.py
@@ -0,0 +1,18 @@
+class Aluno:
+    nome  = ""
+    curso = ""
+    serie = ""
+        
+##############
+
+aluno1 = Aluno()
+aluno1.nome = "Alice"
+aluno1.curso = "Física"
+aluno1.serie = 1
+
+aluno2 = Aluno()
+aluno2.nome = "Patrício"
+aluno2.curso = "Química"
+aluno2.serie = 4
+
+print(f"Seja bem vindo, {aluno2.nome}!")
